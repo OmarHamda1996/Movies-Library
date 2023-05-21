b1835975ec76adb303e7effea6b455986fdb0374
OmarHamada1996 <omer.jasser@gmail.com>
2023-05-21 21:28:47 +0000

    fixing main files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000
+const PORT = 3000
 
 const data= require("./movie data/data.json")
 
@@ -43,8 +43,8 @@ function Movie(title,poster_path,overview){
 
     
 
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 
 
